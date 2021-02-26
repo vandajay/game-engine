119b7524278595fceaeee31b6192b6c89d8d29d6
Kevin Rufino <57162630+kevinrufino@users.noreply.github.com>
2021-02-26 09:40:28 -0500

    passed banana path

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ int main(int argc, char** argv){
 
 	// Make a banana and add to scene. Should update and draw.
 	// Banana* b = new Banana();
-	Sprite* b = new Sprite();
+	Sprite* b = new Sprite("./assets/banana.png");
 	one.addUpdateable(b);
 	one.addDrawable(b);
 	auto b_up = [b](double delta) { b->up(delta); };
